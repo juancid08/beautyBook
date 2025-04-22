fed9c5bce096aa2f0c75ada0e3afc0655d50b8c1
Juancid08 <juan.pizarra.19@gmail.com>
2025-04-22 11:26:48 +0200

    subida de frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './paginas/pagina-principal/pagina-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: PaginaPrincipalComponent
+    }
+];
