f598410cf253058400cc5e3197c54bde09f18178
juancid08 <145454462+juancid08@users.noreply.github.com>
2025-04-30 08:50:48 +0200

    Delete frontend directory

--- frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PaginaPrincipalComponent } from './paginas/pagina-principal/pagina-principal.component';
-
-export const routes: Routes = [{
-    path: '',
-    component: PaginaPrincipalComponent
-    }
-];
